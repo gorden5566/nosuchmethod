c2938bc613e04311b9a2629d9ba2f38a0102492b
gorden5566 <gorden5566@126.com>
2021-04-07 00:48:26 +0800

    client init

---
+++ client/src/main/java/com/gorden5566/Test.java
@@ -0,0 +1,11 @@
+package com.gorden5566;
+
+/**
+ * @author gorden5566
+ * @date 2021/04/07
+ */
+public class Test {
+    public static void main(String[] args) {
+        ShowUtil.show();
+    }
+}
