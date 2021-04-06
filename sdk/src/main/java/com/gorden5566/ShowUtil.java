002f4531cba74db3eaaf1841592b60ced453d8dd
gorden5566 <gorden5566@126.com>
2021-04-07 00:47:15 +0800

    sdk init

---
+++ sdk/src/main/java/com/gorden5566/ShowUtil.java
@@ -0,0 +1,11 @@
+package com.gorden5566;
+
+/**
+ * @author gorden5566
+ * @date 2021/04/07
+ */
+public class ShowUtil {
+    public static void show() {
+        ShapeUtil.show(new Circle());
+    }
+}
