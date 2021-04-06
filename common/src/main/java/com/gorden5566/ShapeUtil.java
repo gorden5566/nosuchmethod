15ae6b3ebe1ade4fd137a3ff4c919cde83803b3c
gorden5566 <gorden5566@126.com>
2021-04-07 00:59:05 +0800

    update common

--- common/src/main/java/com/gorden5566/ShapeUtil.java
+++ common/src/main/java/com/gorden5566/ShapeUtil.java
@@ -5,7 +5,7 @@ package com.gorden5566;
  * @date 2021/04/07
  */
 public class ShapeUtil {
-    public static void show(Shape shape) {
+    public static void show(Circle shape) {
         if (shape != null) {
             shape.show();
         }
